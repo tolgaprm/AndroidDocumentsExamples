30e31951a548896e4ca95ac42021bd8de3ccc4ec
Tolga Pirim <tolgapirim25@gmail.com>
2023-07-19 19:01:52 +0300

    Added per app languages feature

--- app/src/main/java/com/prmto/inviodocuments/MainActivity.kt
+++ app/src/main/java/com/prmto/inviodocuments/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.prmto.inviodocuments
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
 import com.prmto.inviodocuments.databinding.ActivityMainBinding
 
 
@@ -12,5 +17,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setOnItemSelectedListenerLanguageSpinner()
+    }
+
+    private fun setOnItemSelectedListenerLanguageSpinner() {
+        binding.languageSpinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    if (position == 0) {
+                        AppCompatDelegate.setApplicationLocales(LocaleListCompat.forLanguageTags("en"))
+                        Toast.makeText(this@MainActivity, "English", Toast.LENGTH_SHORT).show()
+                    } else {
+                        AppCompatDelegate.setApplicationLocales(LocaleListCompat.forLanguageTags("tr"))
+
+                        Toast.makeText(this@MainActivity, "Turkish", Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Please select a language",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
     }
 }

c14337d035dc529624876d86546963e8e730f1da
Tolga Pirim <tolgapirim25@gmail.com>
2023-07-19 17:10:25 +0300

    Show edit text if locale is tr with config layout

--- app/src/main/java/com/prmto/inviodocuments/MainActivity.kt
+++ app/src/main/java/com/prmto/inviodocuments/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.prmto.inviodocuments
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.prmto.inviodocuments.databinding.ActivityMainBinding
-import java.util.Locale
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,18 +10,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(
-            layoutInflater
-        )
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        showThirdEditTextIfLanguageIsTr()
-    }
-
-    private fun showThirdEditTextIfLanguageIsTr() {
-        val currentLanguage = Locale.getDefault().language
-        if (currentLanguage == "tr") {
-            binding.editTextThird.visibility = View.VISIBLE
-        }
     }
 }

ece4c17d614e3a39292b4362aa31159d733deffe
Tolga Pirim <tolgapirim25@gmail.com>
2023-07-21 15:01:52 +0300

    Added two examples about start multiWindowMode with  using the intent.

--- app/src/main/java/com/prmto/inviodocuments/MainActivity.kt
+++ app/src/main/java/com/prmto/inviodocuments/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.prmto.inviodocuments
 
-import android.content.res.Configuration
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.prmto.inviodocuments.databinding.ActivityMainBinding
 
@@ -13,15 +13,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnRunMultiwindowModeWithYouTube.setOnClickListener {
+            startMultiWindowModeWithYoutubeApp()
+        }
+
+        binding.btnRunMultiWindowModeWithUrl.setOnClickListener {
+            startMultiWindowModeWithUrl()
+        }
     }
 
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
 
-        if (newConfig.uiMode == Configuration.UI_MODE_NIGHT_YES) {
-            Toast.makeText(this, R.string.dark_theme, Toast.LENGTH_SHORT).show()
-        } else if (newConfig.uiMode == Configuration.UI_MODE_NIGHT_NO) {
-            Toast.makeText(this, R.string.light_theme, Toast.LENGTH_SHORT).show()
+    private fun startMultiWindowModeWithYoutubeApp() {
+        Intent(Intent.ACTION_MAIN).apply {
+            `package` = "com.google.android.youtube"
+            addFlags(
+                Intent.FLAG_ACTIVITY_LAUNCH_ADJACENT or
+                        Intent.FLAG_ACTIVITY_NEW_TASK
+            )
+        }.also { intent ->
+            startActivity(intent)
+        }
+    }
+
+    private fun startMultiWindowModeWithUrl() {
+        Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse("https://www.google.com/")
+            addFlags(
+                Intent.FLAG_ACTIVITY_LAUNCH_ADJACENT or
+                        Intent.FLAG_ACTIVITY_NEW_TASK
+            )
+        }.also { intent ->
+            startActivity(intent)
         }
     }
 }
